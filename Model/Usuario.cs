758348366d51036246e03a0ea491b1bf4ab044ba
Harlit <harlit.estevez@gmail.com>
2022-11-28 07:25:14 -0400

    Se hizo validaciones usuario

--- Model/Usuario.cs
+++ Model/Usuario.cs
@@ -8,19 +8,26 @@ namespace RegistroExpedientes.Model
     {
         [Key]
         public int IdUsuario { get; set; }
+        [Required(ErrorMessage = "Ingrese Id Departamento")]
         public int IdDepartamento { get; set; }
+        [Required(ErrorMessage = "Ingrese Nombre")]
         public string Nombre { get; set; } = null!;
+        [Required(ErrorMessage = "Ingrese Apellido")]
         public string Apellido { get; set; } = null!;
+        [Required(ErrorMessage = "Ingrese Cedula")]
         public string Cedula { get; set; } = null!;
+        [Required(ErrorMessage = "Ingrese Usuario")]
         public string Usuario1 { get; set; } = null!;
+        [Required(ErrorMessage = "Ingrese Clave")]
         public string Clave { get; set; } = null!;
+        [Required(ErrorMessage = "Ingrese Privilegio")]
         public string Privilegio { get; set; } = null!;
+        [Required(ErrorMessage = "Ingrese Estado")]
         public bool Estado { get; set; }
+        [Required(ErrorMessage = "Ingrese Fecha")]
         public DateTime PersonCreatedDate { get; set; }
+        [Required(ErrorMessage = "Ingrese Fecha")]
         public DateTime? PersonLastLogin { get; set; }
-
-        //Esto es un comentario para ver si esta haciendo bien la sincronizacion
-
         public virtual Departamento IdDepartamentoNavigation { get; set; } = null!;
     }
 }
