86b19ebe600b8a40577bf790a49d509ae2450deb
Harlit <harlit.estevez@gmail.com>
2022-11-28 07:19:39 -0400

    se agrego validaciones Expedientes

--- Model/Expediente.cs
+++ Model/Expediente.cs
@@ -8,15 +8,26 @@ namespace RegistroExpedientes.Model
     {
         [Key]
         public int IdExpediente { get; set; }
-       
+        [Required(ErrorMessage ="Ingrese Descripcion Expediente")]
         public string DescripcionExpediente { get; set; } = null!;
+        [Required(ErrorMessage ="Ingrese Nombre Beneficiario")]
         public string NombreBeneficiario { get; set; } = null!;
+        [Required(ErrorMessage ="Ingrese Apellido Beneficiario")]
         public string ApellidoBeneficiario { get; set; } = null!;
+        [RegularExpression(@"^\d{3}-\d{7}-\d{1}$")]
+        [Required(ErrorMessage ="Ingrese Cedula")]
         public string CedulaoRnc { get; set; } = null!;
+        [RegularExpression(@"^\d{3}-\d{3}-\d{4}$")]
+        [Phone(ErrorMessage = "Favor de ingresar correctamente el numero Celular.")]
+        [Required(ErrorMessage ="Ingrese Telefono")]
         public string Telefono { get; set; } = null!;
+        [Required(ErrorMessage ="Ingrese Tipo Expediente")]
         public string TipoExpediente { get; set; } = null!;
+        [Required(ErrorMessage ="Ingrese Monto")]
         public decimal Monto { get; set; }
+        [Required(ErrorMessage ="Ingrese Estado")]
         public string Estado { get; set; } = null!;
+        [Required(ErrorMessage = "Ingrese Fecha")]
         public DateTime FechaDeEntrada { get; set; }
     }
 }
