6827f85b297a76644a670a461937e7c450107017
Harlit <harlit.estevez@gmail.com>
2022-12-08 19:15:27 -0400

    prueba de login fallida

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -6,6 +6,8 @@ public class Contexto : DbContext
     
     public DbSet<Expedientes> Expedientes { get; set; }
     public DbSet<Usuarios> Usuarios { get; set; }
+    
+    // public Internal.InternalDbSet<TEntity>.get_EntityType();
     public Contexto(DbContextOptions<Contexto> options) : base(options)
     {
     }
---
+++ Encriptacion/Prueba.cs
@@ -0,0 +1,24 @@
+// public class Prueba
+// {
+//     EncryptMD5 encr = new EncryptMD5();
+//     string mensaje = encr.Encrypt("HOLA");
+
+
+//     Console.writeLine("Ingresa tu usuario: "):
+
+//     string user = Console.ReadLine().Tostring();
+
+
+//     Console.writetine("Ingresa tu contraseña: "):
+
+//     string pass = Console.Readl ine().Tostring();
+//     string lpass = encr.Encrypt(pass);
+//     Console.writeline("La contraseña encriptada es:  + 1pass);
+
+//     Console.writeline("La contraseña desencriptada es: "encr.Decrypt(1pass)); 
+// }
+
+
+
+
+
